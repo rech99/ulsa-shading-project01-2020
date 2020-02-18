566121d894efd07877f9bbc9aa104704883fd1b3
rech99 <rech_99@hotmail.com>
2020-02-18 10:06:50 -0700

    final

---
+++ Assets/Axis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+    float hPos = h * hRange;
+    float vPos = v * vRange;
+
+
+    }
+}
